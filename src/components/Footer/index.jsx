9899ff3a2ff4d53a228160bdf506f347ff38db4b
Matheus Batista <mabatista_10@hotmail.com>
2023-09-26 13:12:29 -0300

    Entrega 1 - Projeto Portfólio

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,10 +1,14 @@
+import styles from "./style.module.css"
+
 export const Footer = () => {
     return(
-        <footer>
+        <footer className={styles.container}>
+            <div className={styles.footerInfo}>
             <h2 className="title2">Contato</h2>
-            <img src="src/assets/whatsapp-icon.png" alt="" />
-            <img src="src/assets/linkedin-icon.png" alt="" />
-            <img src="src/assets/github-icon.png" alt="" />
+            <img className={styles.footerImg} src="src/assets/whatsapp-icon.png" alt="" />
+            <img className={styles.footerImg}  src="src/assets/linkedin-icon.png" alt="" />
+            <img className={styles.footerImg}  src="src/assets/github-icon.png" alt="" />
+            </div>
             <p className="paragraph">Todos os direitos reservados - José da Silva</p>
         </footer>
     )
