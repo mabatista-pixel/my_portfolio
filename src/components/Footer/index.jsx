1ea20b663368a89b4fff97b5af589ecd5e9d4aad
Matheus Batista <mabatista_10@hotmail.com>
2023-09-26 17:11:17 -0300

    Entrega 1 - Projeto Portfolio

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,16 @@
 import styles from "./style.module.css"
 import whatsappIcon from "../../assets/whatsapp-icon.png"
 import linkedinIcon from "../../assets/linkedin-icon.png"
-import githubIcon from "../../github-icon.png"
+import githubIcon from "../../assets/github.png"
 
 export const Footer = () => {
     return(
         <footer className={styles.container}>
             <div className={styles.footerInfo}>
             <h2 className="title2">Contato</h2>
-            <img className={styles.footerImg} src={whatsappIcon} alt="" />
-            <img className={styles.footerImg}  src={linkedinIcon} alt="" />
-            <img className={styles.footerImg}  src={githubIcon} alt="" />
+            <img className={styles.footerImg} src={whatsappIcon} alt="Icone Whatsapp" />
+            <img className={styles.footerImg}  src={linkedinIcon} alt="Icone Linkedin" />
+            <img className={styles.footerImg}  src={githubIcon} alt="Icone Github" />
             </div>
             <p className="paragraph">Todos os direitos reservados - José da Silva</p>
         </footer>
